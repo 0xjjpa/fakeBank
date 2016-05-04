57b23179118cf6fbbeba574b8b0ccd87c0c13a48
Aspos <alibek@gmail.com>
2016-05-04 10:22:22 +0400

    GLOBAL.fxrates added

--- controllers/rates.js
+++ controllers/rates.js
@@ -57,3 +57,64 @@ module.exports.upsert = function* upsert(next) {
         this.throw(405, "Error parsing JSON.");
     }
 };
+
+
+var co = require('co'); //!!!
+
+
+module.exports.doImportRates = function* (app) {
+
+    yield co(function* () {
+        console.log('will try importing fx rates');
+        GLOBAL.fxrates = GLOBAL.fxrates || {};
+
+        var tempfxrates = yield app.db.rates.find({}).exec();
+
+        while (GLOBAL.fxrates.pop()) {};
+        for (var i = 0; i < tempfxrates.length; i++) {
+            GLOBAL.fxrates.push(tempfxrates[i]);
+        }
+
+        return true;
+    }).then(function (value) {
+        console.log('done importing fx rates');
+    }, function (err) {
+        console.error(err.stack);
+    });
+}
+
+
+
+GLOBAL.fxrates = GLOBAL.fxrates || [];
+GLOBAL.fxrates.homecurrency = "EUR"; //???### hardcoded
+
+GLOBAL.fxrates.convertCurrency = function (GivenCur1, GivenAmount, GivenCur2) {
+    //returns how much is GivenAmount of GivenCur2 in GivenCur1
+    function checkIfCurrencyIsKnown(GivenCurrency) {
+        //returns false if given currency code is not present in the fxrates array
+        for (var i = 0; i < GLOBAL.fxrates.length; i++) {
+            if (GLOBAL.fxrates[i].src === GivenCurrency || GLOBAL.fxrates[i].dst === GivenCurrency) return true;
+        }
+        return false;
+    }
+    if (GivenCur1 === GivenCur2) return GivenAmount;
+    if (GLOBAL.fxrates.length < 1) return 0;
+    if (!checkIfCurrencyIsKnown(GivenCur1) || !checkIfCurrencyIsKnown(GivenCur2)) return -1;
+    var Found = false,
+        Result = 0;
+    for (var i = 0; i < GLOBAL.fxrates.length; i++) {
+        if ((GLOBAL.fxrates[i].src === GivenCur1) && (GLOBAL.fxrates[i].dst === GivenCur2)) {
+            Result = GivenAmount * GLOBAL.fxrates[i].sell;
+            Found = true;
+        }
+        if ((GLOBAL.fxrates[i].dst === GivenCur1) && (GLOBAL.fxrates[i].src === GivenCur2)) {
+            Result = GivenAmount / GLOBAL.fxrates[i].buy;
+            Found = true;
+        }
+    }
+    if (Found) return Result;
+    //No direct rate, so will need to do double conversion via fxrates.homecurrency
+    var temp1 = GLOBAL.fxrates.convertCurrency(GLOBAL.fxrates.homecurrency, GivenAmount, GivenCur2); //111222
+    var temp2 = GLOBAL.fxrates.convertCurrency(GivenCur1, temp1, GLOBAL.fxrates.homecurrency);
+    return temp2;
+};
